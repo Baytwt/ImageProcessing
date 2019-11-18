aa0e56a70270c73b80409f8917bbb5453247b347
linjiashen9 <linjiashenwtf@icloud.com>
2019-11-18 20:19:40 +0800

    1

--- Enhance_frequence.py
+++ Enhance_frequence.py
@@ -4,24 +4,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 原图
-img = cv2.imread('./Pics/chrome.jpg',cv2.IMREAD_GRAYSCALE) # 灰度读取
-plt.subplot(231),plt.imshow(img,'gray'),plt.title('original')
+img = cv2.imread('./Pics/apple.jpg',cv2.IMREAD_GRAYSCALE) # 灰度读取
 
 # 二维傅里叶变换,将图像由空间域变换到频域
 fft2 = np.fft.fft2(img)
-plt.subplot(232),plt.imshow(np.abs(fft2),'gray'),plt.title('fft2')
 
-# 将图像变换的原点移动到频域矩阵的中心,并显示结果
-shift2center = np.fft.fftshift(fft2)
-plt.subplot(233),plt.imshow(np.abs(shift2center),'gray'),plt.title('shift2center')
+# 将图像变换的原点移动到频域矩阵的中心
+fshift = np.fft.fftshift(fft2)
 
-# 对傅里叶变换的结果进行对数变换
-log_fft2 = np.log(1 + np.abs(fft2))
-plt.subplot(235),plt.imshow(log_fft2,'gray'),plt.title('log_fft2')
+# 低通滤波器
 
-# 对中心化后的结果进行对数变换
-log_shift2center = np.log(1 + np.abs(shift2center))
-plt.subplot(236),plt.imshow(log_shift2center,'gray'),plt.title('shift2center')
+# 在频域取d将其反变换到空间域
+def make_transform_matrix(d,image):
+    transfor_matrix = np.zeros(image.shape)
+    center_point = tuple(map(lambda x:(x-1)/2, fshift.shape))
+    for i in range(transfor_matrix.shape[0]):
+        for j in range(transfor_matrix.shape[1]):
+            def cal_distance(pa,pb):
+                from math import sqrt
+                dis = sqrt((pa[0]-pb[0])**2 + (pa[1]-pb[1])**2)
+                return dis
+            dis = cal_distance(center_point, (i,j))
+            if dis <= d:
+                transfor_matrix[i,j] = 1
+            else:
+                transfor_matrix[i,j] = 0
+    return transfor_matrix
+
+d_1 = make_transform_matrix(10,fshift)
+d_2 = make_transform_matrix(20,fshift)
+d_3 = make_transform_matrix(50,fshift)
+
+# 低通滤波
+img_d1 = np.abs(np.fft.ifft2(np.fft.ifftshift(fshift*d_1)))
+img_d2 = np.abs(np.fft.ifft2(np.fft.ifftshift(fshift*d_2)))
+img_d3 = np.abs(np.fft.ifft2(np.fft.ifftshift(fshift*d_3)))
+
+plt.subplot(231),plt.imshow(img,'gray'),plt.title('origin')
+plt.subplot(232),plt.imshow(img_d1,'gray'),plt.title('d=10')
+plt.subplot(233),plt.imshow(img_d2,'gray'),plt.title('d=20')
+plt.subplot(234),plt.imshow(img_d3,'gray'),plt.title('d=50')
+
+# # 对傅里叶变换的结果进行对数变换 目的是将数据变化到0~255
+# log_fft2 = np.log(1 + np.abs(fft2))
+# plt.subplot(235),plt.imshow(log_fft2,'gray'),plt.title('log_fft2')
+#
+# # 对中心化后的结果进行对数变换
+# log_shift2center = np.log(1 + np.abs(fshift))
+# plt.subplot(236),plt.imshow(log_shift2center,'gray'),plt.title('fshift')
 
 
 
